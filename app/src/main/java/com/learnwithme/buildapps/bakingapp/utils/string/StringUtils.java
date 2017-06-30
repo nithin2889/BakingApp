04c9ab38b6229cd4df609d41705369c1baac2e9a
Nithin Prasad <nithin2889@gmail.com>
2017-06-29 21:10:46 -0500

    Added layout files, utility files and modified manifest.

--- app/src/main/java/com/learnwithme/buildapps/bakingapp/utils/string/StringUtils.java
+++ app/src/main/java/com/learnwithme/buildapps/bakingapp/utils/string/StringUtils.java
@@ -2,6 +2,10 @@ package com.learnwithme.buildapps.bakingapp.utils.string;
 
 import android.content.Context;
 
+import com.learnwithme.buildapps.bakingapp.R;
+
+import java.util.Locale;
+
 /**
  * Created by Nithin on 26/06/2017.
  */
@@ -9,7 +13,11 @@ import android.content.Context;
 public class StringUtils {
     public static String formatIngredientString(Context context, String name,
                                                 float quantity, String measure) {
-
-        return null;
+        String line = context.getResources().getString(R.string.recipe_details_ingredient_line);
+        String quantityStr = String.format(Locale.US, "%s", quantity);
+        if(quantity == (long) quantity) {
+            quantityStr = String.format(Locale.US, "%d", (long) quantity);
+        }
+        return String.format(Locale.US, line, name, quantityStr, measure);
     }
 }
